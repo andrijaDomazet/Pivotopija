04879807e548e509eac30332d8b593c915e74234
Galax Svemir <MOZAK@Galaxs-Mac-mini.local>
2019-04-16 15:54:12 +0200

    dodatak za master koji se ne preporucuje

---
+++ src/components/Lender/Lender.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import BeerLady1 from './lenderIMG/BeerLady1.png';
+import staripapir from './lenderIMG/staripapir.png';
+import plutoDIV from './lenderIMG/plutoDIV.png';
+import srednjilogo from './lenderIMG/srednjilogo.png';
+import {Spring} from 'react-spring';
+
+
+
+export default function Lender() {
+  return (
+   
+    <div className='Lstyle'>   
+
+      <div className='lady'>  
+          <img src={BeerLady1} width= '100%' alt='slika2'/>
+      </div>
+
+      <div className='srednjilogo'>  
+          <img src={srednjilogo} alt='slika3'/>
+      </div>
+
+      <div className='pluto'>  
+          <img src={plutoDIV} alt='slika4'/>
+      </div>
+
+      <div className='papir'>  
+          <img src={staripapir} alt='slika1'/>
+      </div>
+      
+    </div>
+  )
+}
--- src/components/Mainscreen/Mainscreen.js
+++ src/components/Mainscreen/Mainscreen.js
@@ -5,6 +5,8 @@ import BottomCover from "../Bottom_menu/Bottom_menu";
 import cover from "../../img/cover.jpg";
 import trougao from "../../img/trougao.png";
 import bottle from "../../img/bottle.png";
+import Lender from '../Lender/Lender';
+import srednjilogo from '../Lender/lenderIMG/srednjilogo.png';
 
 export default class Mainscreen extends Component {
   state = {
@@ -20,9 +22,9 @@ export default class Mainscreen extends Component {
       <React.Fragment>
         <div className="main_cover">
           <img src={cover} alt="" width="100%" height="100%" />
-          <p>Idemmmooo...</p>
+          <Lender />
           <button className="btn_zanatskaPiva" onClick={this.toggle2}>
-            <img src={trougao} alt="" width="120rem" />
+            <img src={srednjilogo} alt="" width="75%" />
           </button>
           <div className="btn_bottle">
             <img src={bottle} alt="" height="400rem" />
