ba25267506f9466fe03a0d1a4899c8000862e257
Andrija Domazet <domazet.andrija@gmail.com>
2019-04-16 23:52:47 +0200

    Ovo vam nije dobar primer, ali ajde...

--- src/components/Lender/Lender.js
+++ src/components/Lender/Lender.js
@@ -1,33 +1,25 @@
-import React from 'react';
-import BeerLady1 from './lenderIMG/BeerLady1.png';
-import staripapir from './lenderIMG/staripapir.png';
-import plutoDIV from './lenderIMG/plutoDIV.png';
-import srednjilogo from './lenderIMG/srednjilogo.png';
-import {Spring} from 'react-spring';
-
-
+import React from "react";
+import BeerLady1 from "./lenderIMG/BeerLady1.png";
+import staripapir from "./lenderIMG/staripapir.png";
+import plutoDIV from "./lenderIMG/plutoDIV.png";
+import srednjilogo from "./lenderIMG/srednjilogo.png";
+import "./Lender.scss";
+import { Spring } from "react-spring";
 
 export default function Lender() {
   return (
-   
-    <div className='Lstyle'>   
-
-      <div className='lady'>  
-          <img src={BeerLady1} width= '100%' alt='slika2'/>
+    <div className="Lstyle">
+      <div className="lady">
+        <img src={BeerLady1} width="100%" alt="slika2" />
       </div>
 
-      <div className='srednjilogo'>  
-          <img src={srednjilogo} alt='slika3'/>
+      <div className="pluto">
+        <img src={plutoDIV} alt="slika4" />
       </div>
 
-      <div className='pluto'>  
-          <img src={plutoDIV} alt='slika4'/>
+      <div className="papir">
+        <img src={staripapir} alt="slika1" />
       </div>
-
-      <div className='papir'>  
-          <img src={staripapir} alt='slika1'/>
-      </div>
-      
     </div>
-  )
+  );
 }
