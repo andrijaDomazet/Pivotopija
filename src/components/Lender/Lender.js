9cb8779dc6ba8d442451d1009a31115a3812f372
Andrija Domazet <domazet.andrija@gmail.com>
2019-04-19 11:48:31 +0200

    jos neke promene

--- src/components/Lender/Lender.js
+++ src/components/Lender/Lender.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import BeerLady1 from "../Lender/lenderIMG/BeerLady1.png";
 import pluto from "../Lender/lenderIMG/pluto.png";
 import srednjiLogo from "../Lender/lenderIMG/srednjilogo.png";
 import "../Lender/Lender.scss";
+import cover from "../../img/cover.jpg";
 import { Spring } from "react-spring";
 
-export default function Lender() {
-  return (
-    <div className="Lstyle">
-      {/* <div className="Lady1">
-        <img src={BeerLady1} alt="slika2" />
-      </div> */}
+export default class Lender extends Component {
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     prikaz_objekata: false
+  //   };
+  // }
+  // toggle2 = () => {
+  //   this.props.history.push("/objekti");
+  // };
 
-      {/*<div className='srednjiLogo'>
-          <img src={srednjiLogo}  alt='slika2'/>
-        </div> */}
-
-      {/* <div className="pluto">
-        <img src={pluto} alt="slika3" />
-      </div> */}
-    </div>
-  );
+  render() {
+    return (
+      <div className="main_cover">
+        <img src={cover} alt="" width="100%" height="100%" />
+        <div className="Lady1">
+          <img src={BeerLady1} width="25%" alt="slika2" />
+        </div>
+        <div className="pluto">
+          <img src={pluto} width="30%" alt="slika3" />
+        </div>
+        <button className="btn_zanatskaPiva" onClick={this.props.toggle2}>
+          <img src={srednjiLogo} width="200px" alt="" />
+        </button>
+      </div>
+    );
+  }
 }
