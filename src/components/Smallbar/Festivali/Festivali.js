7969b5011a4d8e63f0a37f7ba0a9929664417f69
initiateseven <initiateseven@gmail.com>
2019-04-26 17:18:38 +0200

    Promenjene Vrste u Festivali

---
+++ src/components/Smallbar/Festivali/Festivali.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import Festivali19 from './Festivali19/Festivali19.js';
+import Smallbar from '../Smallbar';
+import './Festivali.scss';
+
+export default class Festivali extends Component {
+  render() {
+    return (
+      <div>
+    <Smallbar />
+    <div className='fstyle'>
+      
+      <Festivali19/>
+
+       </div>
+    </div>);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
