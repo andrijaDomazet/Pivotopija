1d1dcd233618fc4ddf8859a7711ff758f5f37723
andrijaDomazet <48025495+andrijaDomazet@users.noreply.github.com>
2019-11-28 21:54:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ class App extends Component {
         <section style={pozadina}>
           <Navbar />
           <Switch>
-            <Route exact path="andrijadomazet.github.io/Pivotopija/"/>
+            <Route exact path="https://andrijadomazet.github.io/Pivotopija/"/>
             <Route path="/" exact component={Mainscreen} />
             <Route path="/info" component={Info} />
             <Route path="/piva" component={ListaPiva} />
