e1082390c8a4f1dca731d9b88891ab5454bc6210
andrijaDomazet <domazet.andrija@gmail.com>
2020-03-25 20:00:25 +0100

    Iproving slidedrawer functionality

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { Component } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Topbar from "./components/Topbar/Topbar";
 import Navbar from "./components/Navbar/Navbar";
-import Background from "../src/img/pozadina.jpg";
 import ListaObjekata from "./components/Objekti/ListaObjekata";
 import Mainscreen from "./components/Mainscreen/Mainscreen";
 import Info from "./components/Info/Info";
@@ -14,40 +13,29 @@ import Postupak from "./components/Smallbar/Postupak/Postupak.js";
 import Predstavljamo from "./components/Smallbar/Predstavljamo/Predstavljamo.js";
 import Footer from "./components/Footer/Footer.js";
 import Kontakt from "./components/Topbar/Kontakt/Kontakt.js";
-import "./App";
+import "./App.scss";
 
-class App extends Component {
-  render() {
-    var pozadina = {
-      width: "100%",
-      height: "100%",
-      marginTop: "1rem",
-      backgroundImage: `url(${Background})`
-    };
-
-    return (
-      <Router style={{ height: "100%" }}>
+export default function App() {
+  return (
+    <div className="app">
+      <Router>
         <Topbar />
-        <section style={pozadina}>
-          <Navbar />
-          <Switch>
-            {/* <Route exact path="https://andrijadomazet.github.io/Pivotopija/" /> */}
-            <Route path="/" exact component={Mainscreen} />
-            <Route path="/info" component={Info} />
-            <Route path="/piva" component={ListaPiva} />
-            <Route path="/objekti" component={ListaObjekata} />
-            <Route path="/vrste" component={Vrste} />
-            <Route path="/festivali" component={Festivali} />
-            <Route path="/postupak" component={Postupak} />
-            <Route path="/predstavljamo" component={Predstavljamo} />
-            <Route path="/Kontakt" component={Kontakt} />
-            <Route component={Mainscreen} />
-          </Switch>
-          <Footer />
-        </section>
+        <Navbar />
+        <Switch>
+          {/* <Route exact path="https://andrijadomazet.github.io/Pivotopija/" /> */}
+          <Route path="/" exact component={Mainscreen} />
+          <Route path="/info" component={Info} />
+          <Route path="/piva" component={ListaPiva} />
+          <Route path="/objekti" component={ListaObjekata} />
+          <Route path="/vrste" component={Vrste} />
+          <Route path="/festivali" component={Festivali} />
+          <Route path="/postupak" component={Postupak} />
+          <Route path="/predstavljamo" component={Predstavljamo} />
+          <Route path="/Kontakt" component={Kontakt} />
+          <Route component={Mainscreen} />
+        </Switch>
+        <Footer />
       </Router>
-    );
-  }
+    </div>
+  );
 }
-
-export default App;
